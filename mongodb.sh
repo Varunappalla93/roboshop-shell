96d2deaaeb94f05f2355e93b8f8e7ed43ea6b6ed
Varunappalla93 <an.varun1993@gmail.com>
2025-02-13 16:41:50 +0530

    MongoDB  Script

--- mongodb.sh
+++ mongodb.sh
@@ -47,7 +47,7 @@ VALIDATE $? "Enabling MongoDB"
 systemctl start mongod &>> $LOGFILE
 VALIDATE $? "Starting MongoDB"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' etc/mongod.conf &>> $LOGFILE
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
 VALIDATE $? "Remote access to MongoDB"
 
 systemctl restart mongod
