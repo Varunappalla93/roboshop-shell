4ccda9fe582023aa68c4747c148b3a8507c70bce
Varunappalla93 <an.varun1993@gmail.com>
2025-02-13 16:02:40 +0530

    MongoDB  Script

--- mongodb.sh
+++ mongodb.sh
@@ -1,1 +1,39 @@
 #!/bin/bash
+
+
+ID=$(id -u) 
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Script started executing at $TIMESTAMP" &>>$LOGFILE
+
+
+VALIDATE()
+{
+if [ $1 -ne 0 ]
+    then
+        echo -e "ERROR:: $2 is $R failed $N"
+        exit 1
+    else
+        echo -e "$2 is $G successful $N"
+fi
+}
+
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R Error, pls try with root user $N"
+    exit 1 # non-zero
+else
+   echo "you are root user"
+fi
+
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+VALIDATE $? "Copied MongoDB Repo"
