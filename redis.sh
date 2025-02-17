aca0b94052116f3eaf26b15e65537c18a90e49f0
Varunappalla93 <an.varun1993@gmail.com>
2025-02-17 14:11:59 +0530

    redis

---
+++ redis.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+ID=$(id -u) 
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+exec &>LOGFILE
+
+echo "Script started executing at $TIMESTAMP" &>>$LOGFILE
+
+
+VALIDATE()
+{
+if [ $1 -ne 0 ]
+    then
+        echo -e "ERROR:: $2 is $R failed $N"
+        exit 1
+    else
+        echo -e "$2 is $G successful $N"
+fi
+}
+
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R Error, pls try with root user $N"
+    exit 1 # non-zero
+else
+   echo "you are root user"
+fi
+
+
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y 
+VALIDATE $? "installing remi release"
+
+dnf module enable redis:remi-6.2 -y 
+VALIDATE $? "emabling redis"
+
+dnf install redis -y 
+VALIDATE $? "installing redis"
+
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf 
+VALIDATE $? "allowing remote connections"
+
+systemctl enable redis
+VALIDATE $? "enable redis"
+
+systemctl start redis 
+VALIDATE $? "start redis"
