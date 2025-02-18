020928a9b32804c6f7576d6bc45f6ab579560c98
Varunappalla93 <an.varun1993@gmail.com>
2025-02-18 15:19:27 +0530

    added logs for user

--- user.sh
+++ user.sh
@@ -34,13 +34,13 @@ else
    echo "you are root user"
 fi
 
-dnf module disable nodejs -y &>> LOGFILE
+dnf module disable nodejs -y &>> $LOGFILE
 VALIDATE $? "disabling current nodejs"
 
-dnf module enable nodejs:18 -y &>> LOGFILE
+dnf module enable nodejs:18 -y &>> $LOGFILE
 VALIDATE $? "enabling current nodejs"
 
-dnf install nodejs -y &>> LOGFILE
+dnf install nodejs -y &>> $LOGFILE
 VALIDATE $? "installing current nodejs"
 
 id roboshop
@@ -52,40 +52,39 @@ else
     echo -e "roboshop user already exists $Y Skipping $N"
 fi
 
-mkdir -p /app &>> LOGFILE
+mkdir -p /app &>> $LOGFILE
 VALIDATE $? "creating app directory"
 
-curl -L -o /tmp/user.zip https://roboshop-builds.s3.amazonaws.com/user.zip &>> LOGFILE
+curl -L -o /tmp/user.zip https://roboshop-builds.s3.amazonaws.com/user.zip &>> $LOGFILE
 VALIDATE $? "Downloading user app"
 
-cd /app 
-unzip /tmp/user.zip &>> LOGFILE
-
+cd /app &>> $LOGFILE
+unzip /tmp/user.zip &>> $LOGFILE
 VALIDATE $? "unzipping user app"
 
-npm install  &>> LOGFILE
+npm install  &>> $LOGFILE
 VALIDATE $? "installing dependencies"
 
-cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service
+cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service &>> $LOGFILE
 VALIDATE $? "copying user service file"
 
-systemctl daemon-reload &>> LOGFILE
+systemctl daemon-reload &>> $LOGFILE
 VALIDATE $? "user daemon reload" 
 
-systemctl enable user &>> LOGFILE
+systemctl enable user &>> $LOGFILE
 VALIDATE $? "user daemon enable"
 
-systemctl start user &>> LOGFILE
+systemctl start user &>> $LOGFILE
 VALIDATE $? "user daemon start"
 
-cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 VALIDATE $? "copying mongo repo"
 
-dnf install mongodb-org-shell -y &>> LOGFILE
-VALIDATE $? "installing mongodb shell"
+dnf install mongodb-org-shell -y &>> $LOGFILE
+VALIDATE $? "installing mongodb client"
 
 
-mongo --host mongodb.appalla.shop </app/schema/user.js &>> LOGFILE
+mongo --host mongodb.appalla.shop </app/schema/user.js &>> $LOGFILE
 VALIDATE $? "loading user data into mongodb"
 
 
